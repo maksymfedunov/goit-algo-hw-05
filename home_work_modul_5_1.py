73aa95c7c45f65ee5770d33d0a32da1d87c73ed5
maksymfedunov <maksim.kherson88@gmail.com>
2025-03-20 22:51:28 +0100

    first_commit

--- home_work_modul_5_1.py
+++ home_work_modul_5_1.py
@@ -14,10 +14,5 @@ def caching_fibonacci():
 
     return fibonacci #Возвращаем функцию
 
-# Отримуємо функцію fibonacci
-fib = caching_fibonacci()
 
-# Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
-print(fib(10))  # Виведе 55
-print(fib(15))  # Виведе 610
 
